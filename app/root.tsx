e9baf95861171560f2c4429e58a1b1cb6107fcbe
merzainc <h200649z@hit.ac.zw>
2024-12-10 10:32:22 +0200

    fix: update meta data

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="App description" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
